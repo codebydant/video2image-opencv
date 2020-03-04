9d72b82ed19bb38bc183f91872686d042b6c3ea9
Daniel <35694200+danielTobon43@users.noreply.github.com>
2020-03-04 15:45:41 +0000

    Add files via upload

--- src/video2image.cpp
+++ src/video2image.cpp
@@ -18,7 +18,7 @@ int main(int argc, const char *argv[]) {
     std::cout << "*********************" << std::endl;
 
     std::vector<std::string> images_dir;
-    std::cout << "-> Getting images sequence from video..." << std::endl;
+    std::cout << "-> Getting images sequence from video...please wait" << std::endl;
 
     ////////////////////input image sequences//////////////////////
     // std::vector<cv::Mat> video_input;
@@ -33,27 +33,57 @@ int main(int argc, const char *argv[]) {
       std::exit(-1);
     }
 
+    // capture.set(cv::CAP_PROP_POS_AVI_RATIO, 1);
+
     frameNumber = capture.get(cv::CAP_PROP_FRAME_COUNT);
     reference_number = (frameNumber - 1) / 2;
+    float fps = capture.get(cv::CAP_PROP_FPS);
+    // float duration = frameNumber / fps;
+
+    // std::cout << "fps: " << fps << std::endl;
+    // std::cout << "Number of frames: " << frameNumber << std::endl;
+
+    int multiplier = fps * 2;
 
     std::cout << "-------------------------------------" << std::endl;
-    cv::namedWindow("inputVideo", cv::WINDOW_AUTOSIZE);
-    for (size_t frame_i = 0; frame_i < frameNumber; frame_i++) {
-      capture >> currentFrame;
-      if (frame_i == reference_number) {
-        referFrame = currentFrame.clone();
-      }
 
-      if (currentFrame.empty()) {
-        std::cout << "-> frame empty: " << std::endl;
-        break;
+    // cv::namedWindow("inputVideo", cv::WINDOW_AUTOSIZE);
+    int cont = 0;
+    for (size_t frame_i = 0; frame_i < frameNumber; frame_i++) {
+
+      // while (capture.isOpened()) {
+      int frameId = frame_i;
+      float frameI_msc = capture.get(cv::CAP_PROP_POS_MSEC);
+      // std::cout << "-> frame id: " << frameId << std::endl;
+      // std::cout << "-> msc id: " << frameI_msc << std::endl;
+      capture.set(cv::CAP_PROP_POS_FRAMES, frameId);
+      if (((int)frameId % multiplier) == 0) {
+        capture >> currentFrame;
+        // if (frame_i == reference_number) {
+        //   referFrame = currentFrame.clone();
+        // }
+        // std::cout << "-> frame id: " << frameId << std::endl;
+
+        if (currentFrame.empty()) {
+          std::cout << "-> frame empty: " << std::endl;
+          break;
+        }
+
+        // video_input.push_back(currentFrame.clone());
+        // std::string filename = "Image_" + std::to_string((int)frame_i) + ".png";
+        std::string filename = "Image_" + std::to_string(cont) + ".png";
+        cv::imwrite(filename.c_str(), currentFrame.clone());
+        // cv::namedWindow("inputVideo", cv::WINDOW_NORMAL);
+        // cv::resizeWindow("inputVideo", 640, 480);
+        // cv::imshow("inputVideo", currentFrame);
+        std::cout << "-> Saved: " << filename << std::endl;
+        cont += 1;
+        cv::waitKey(400);
+        cv::destroyAllWindows();
+
+      } else {
+        continue;
       }
-      // video_input.push_back(currentFrame.clone());
-      std::string filename = "Image_" + std::to_string((int)frame_i) + ".jpg";
-      cv::imwrite(filename.c_str(), currentFrame.clone());
-      cv::imshow("inputVideo", currentFrame);
-      std::cout << "-> Saved: " << filename << std::endl;
-      cv::waitKey(400);
     }
     capture.release();
     std::cout << "-------------------------------------" << std::endl;
